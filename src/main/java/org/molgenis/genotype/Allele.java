a28ceb13f9d6e57c0e09a2896db23398da2c18e0
Dennis Hendriksen <d.hendriksen@umcg.nl>
2018-12-21 15:22:34 +0100

    Extract VCF IO from GenotypeIO

---
+++ src/main/java/org/molgenis/genotype/Allele.java
@@ -0,0 +1,147 @@
+package org.molgenis.genotype;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.molgenis.genotype.util.Utils;
+
+public final class Allele implements Comparable<Allele> {
+
+  private static Map<String, Allele> pool = new HashMap<String, Allele>();
+  private static Map<Character, Allele> snpPool = new HashMap<Character, Allele>();
+
+  public static final Allele A = create('A');
+  public static final Allele C = create('C');
+  public static final Allele G = create('G');
+  public static final Allele T = create('T');
+  public static final Allele ZERO = create('0');
+
+  private final String allele;
+  private final char snpAllele;
+  private Allele complement;
+  private final int hashCode;
+
+  private Allele(String allele) {
+    if (allele.length() == 1) {
+      if (allele.charAt(0) == 'A'
+          || allele.charAt(0) == 'C'
+          || allele.charAt(0) == 'G'
+          || allele.charAt(0) == 'T'
+          || allele.charAt(0) == '0') {
+        this.snpAllele = allele.charAt(0);
+      } else {
+        // Some times these are found in plink files. These are not SNPs
+        this.snpAllele = (char) -1;
+      }
+
+    } else {
+      this.snpAllele = (char) -1;
+    }
+    this.allele = allele;
+    this.hashCode = allele.hashCode();
+  }
+
+  private Allele(char allele) {
+    this(String.valueOf(allele));
+  }
+
+  public boolean isSnpAllele() {
+    return (byte) snpAllele != -1;
+  }
+
+  /** @return the allele */
+  public String getAlleleAsString() {
+    return allele;
+  }
+
+  /** @return the snpAllele */
+  public char getAlleleAsSnp() {
+    return snpAllele;
+  }
+
+  private void addComplement(Allele complement) {
+    this.complement = complement;
+  }
+
+  public Allele getComplement() {
+    if (isSnpAllele()) {
+      return complement;
+    } else {
+      throw new RuntimeException("Complement currenlty only supported for SNPs");
+    }
+  }
+
+  public static Allele create(String alleleString) {
+
+    if (alleleString == null) {
+      return ZERO;
+    }
+
+    if (alleleString.isEmpty()) {
+      return ZERO;
+    }
+
+    Allele oldAllele = pool.get(alleleString);
+    if (oldAllele != null) {
+      return oldAllele;
+    } else {
+
+      // Do this to make sure not to save whole line in background after a split or tokenizer
+      alleleString = new String(alleleString);
+      Allele newAllele = new Allele(alleleString);
+      pool.put(alleleString, newAllele);
+      if (newAllele.isSnpAllele()) {
+        snpPool.put(newAllele.getAlleleAsSnp(), newAllele);
+        newAllele.addComplement(
+            Allele.create(Utils.getComplementNucleotide(newAllele.getAlleleAsSnp())));
+      }
+      return newAllele;
+    }
+  }
+
+  public static Allele create(char alleleChar) {
+
+    if (alleleChar == '\0') {
+      return Allele.ZERO;
+    }
+
+    Allele oldAllele = snpPool.get(alleleChar);
+    if (oldAllele != null) {
+      return oldAllele;
+    } else {
+      Allele newAllele = new Allele(alleleChar);
+      snpPool.put(alleleChar, newAllele);
+      pool.put(newAllele.getAlleleAsString(), newAllele);
+      if (newAllele.isSnpAllele()) {
+        newAllele.addComplement(Allele.create(Utils.getComplementNucleotide(alleleChar)));
+      }
+      return newAllele;
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return hashCode;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Allele other = (Allele) obj;
+    if (allele == null) {
+      if (other.allele != null) return false;
+    } else if (!allele.equals(other.allele)) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return this.getAlleleAsString();
+  }
+
+  @Override
+  public int compareTo(Allele other) {
+    return this.allele.compareTo(other.allele);
+  }
+}
---
+++ src/main/java/org/molgenis/vcf/VcfReader.java
@@ -0,0 +1,72 @@
+package org.molgenis.vcf;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+import java.util.Iterator;
+import net.sf.samtools.util.BlockCompressedInputStream;
+import org.molgenis.vcf.meta.VcfMeta;
+import org.molgenis.vcf.meta.VcfMetaParser;
+
+/**
+ * A high-performance VCF reader based on the the Variant Call Format (VCF) Version 4.2
+ * Specification
+ *
+ * <p>In order to achieve high performance VcfRecord and VcfSample objects are recycled, use
+ * createClone() if you need to store these object in a iteration.
+ */
+public class VcfReader implements Iterable<VcfRecord>, Closeable {
+  private final BufferedReader reader;
+  private final BlockCompressedInputStream blockCompressedInputStream;
+  private VcfMeta vcfMeta;
+
+  public VcfReader(Reader reader) {
+    if (reader == null) throw new IllegalArgumentException("reader is null");
+    this.reader =
+        reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader);
+    this.blockCompressedInputStream = null;
+  }
+
+  public VcfReader(BlockCompressedInputStream blockCompressedInputStream) {
+    if (blockCompressedInputStream == null)
+      throw new IllegalArgumentException("blockCompressedInputStream is null");
+    this.blockCompressedInputStream = blockCompressedInputStream;
+    this.reader = null;
+  }
+
+  @Override
+  public Iterator<VcfRecord> iterator() {
+    if (vcfMeta == null) {
+      try {
+        vcfMeta = parseVcfMeta();
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }
+    VcfRecordReader vcfRecordReader =
+        reader != null
+            ? new VcfRecordReader(reader, vcfMeta)
+            : new VcfRecordReader(blockCompressedInputStream, vcfMeta);
+    return vcfRecordReader.iterator();
+  }
+
+  public VcfMeta getVcfMeta() throws IOException {
+    if (vcfMeta == null) {
+      vcfMeta = parseVcfMeta();
+    }
+    return vcfMeta;
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (reader != null) reader.close();
+    else if (blockCompressedInputStream != null) blockCompressedInputStream.close();
+  }
+
+  private VcfMeta parseVcfMeta() throws IOException {
+    if (reader != null) return new VcfMetaParser(reader).parse();
+    else return new VcfMetaParser(blockCompressedInputStream).parse();
+  }
+}
